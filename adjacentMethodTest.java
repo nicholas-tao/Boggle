595943145c3cb64fe426f386577f9d3f384657ef
Rohan Ravindran <rohan.ravindran24@gmail.com>
2019-06-02 19:05:40 -0400

    Update adjacentMethodTest.java

--- adjacentMethodTest.java
+++ adjacentMethodTest.java
@@ -1,86 +1,90 @@
 import java.util.*;
 
-public class adjacentMethodTest {
+public class testMethod {
 	public static void main(String args[]) {
 		
-		String[][] input = {{"R", "A", "D", "E", "L"}, 
-							{"B", "O", "W", "P", "O"}, 
-							{"L", "O", "W", "W", "T"}, 
-							{"L", "O", "W", "L", "T"}, 
-							{"L", "O", "W", "W", "A"}};
+		String[][] input = {{"B", "A", "D", "E", "L"}, 
+							{"Q", "D", "W", "P", "O"}, 
+							{"X", "I", "C", "N", "T"}, 
+							{"L", "O", "F", "A", "S"}, 
+							{"V", "H", "K", "S", "N"}};
 		
-		int[] coordinates = new int[2]; // can create an arraylist to deal with multiple occurances of same letter
-		String word = "DWIOZ";		
-		coordinates = getFirst(input, word);
+		String word = "ADIOH";
+		System.out.println(checkAdjacent(input, word));
 		
-		int x = coordinates[0];
-		int y = coordinates[1];
-		
-		System.out.println(checkAdjacent(input, coordinates, word, 1, false));
-
 	}
-	
-	public static int[] getFirst(String input[][], String word) {
 		
-		int[] coordinates = new int[2];
-		for (int i=0; i<input.length; i++) { //fix if can't find first letter
+	public static boolean checkAdjacent(String input[][], String word) {
+		ArrayList<Integer> coordinates = new ArrayList<Integer>();
+		for (int i=0; i<input.length; i++) {
 			for (int j=0; j<input[i].length; j++) {
-				if (input[i][j].equals(word.substring(0, 1))) {
-					coordinates[0] = i;
-					coordinates[1] = j;
+				if (input[i][j].equals(Character.toString(word.charAt(0)))) {
+					System.out.println("Starting Coordinates:" + i + ", " +j);
+					coordinates.add(i);
+					coordinates.add(j);
 				}
 			}
 		}
 		
-		return coordinates;
+		if (coordinates.size() == 0) return false;
 		
+		int counter = 0;
+		for (int i=0; i<coordinates.size()/2; i++) {
+			int[] coordinatesArray = {coordinates.get(counter), coordinates.get(counter+1)};
+			if (getAdjacent(input, word, coordinatesArray)) {
+				return true;
+			}
+			counter += 2;
+		}
+		return false;
 	}
 	
-	public static boolean checkAdjacent(String[][] input, int[] coordinates, String word, int wordCount, boolean letterFound) {
-		if (wordCount > word.length()-1) {
+	public static boolean getAdjacent(String[][] input, String word, int[] startingCoordinates) {
+		int[] parentCoordinates = startingCoordinates;
+		int[] row = {-1, -1, -1, 0, 0, 1, 1, 1};
+		int[] column = {-1, 0, 1, -1, 1, -1, 0, 1}; 
+		boolean charFound = true;
+		int charCounter = 0;
+		
+		if (word.length() == 1) {
 			return true;
 		}
-			letterFound = false;
-			String letter = Character.toString(word.charAt(wordCount));
-			System.out.println(letter);
-			
-			int[] s = {-1, 0, 1};
-			
-			for (int i=0; i<3; i++) {
-				int column = coordinates[1] + s[i];
-				
-
-				if (column < 0 || column >= input.length) {
-					continue;
-				} else {
-					for (int j=0; j<3; j++) {
-						
-						int row = coordinates[0] + s[j];
-						if (row < 0 || row >= input.length) {
-							continue;
-						} else {
-							if (input[row][column].equals(letter)) {
-								
-								System.out.println(row + ", " + column);
-								coordinates[0] = row;
-								coordinates[1] = column;
-								System.out.println("Word Count: "+wordCount);
-								letterFound = true;
-								wordCount += 1;
-								
-								checkAdjacent(input, coordinates, word, wordCount, letterFound);
-							}
-					}
+		
+		for (int j=1; j<word.length(); j++) {
+			String currentLetter = Character.toString(word.charAt(j));
+			if (charFound) {
+				charFound = false;
+				for (int i=0; i<8; i++) {
+					int xPos = startingCoordinates[0] + row[i];
+					int yPos = startingCoordinates[1] + column[i];
 					
+					if (xPos < 0 || yPos < 0 || xPos >= input.length || yPos >= input.length) {
+						continue;
+					} else {
+						System.out.println(xPos + ", " + yPos);
+						String newPos = xPos + " " + yPos;
+						String parentPos = parentCoordinates[0] + " " + parentCoordinates[1];
+						if (input[xPos][yPos].equals(currentLetter) && (!newPos.equals(parentPos))) {
+							System.out.println(currentLetter);
+							parentCoordinates[0] = startingCoordinates[0];
+							parentCoordinates[1] = startingCoordinates[1];
+							startingCoordinates[0] = xPos;
+							startingCoordinates[1] = yPos;
+							charFound = true;
+							break;
+						}
+					}
 				}
+			} else {
+				break;
 			}
-			
+			charCounter++;
 		}
-		if (letterFound == true) {
+		
+		if (charCounter == word.length()-1 && charFound == true) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
 }
