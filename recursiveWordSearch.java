883c5dccf35028193a432d00dfd47ba41a8428ce
Rohan Ravindran <rohan.ravindran24@gmail.com>
2019-06-04 22:18:32 -0400

    Update recursiveWordSearch.java

--- recursiveWordSearch.java
+++ recursiveWordSearch.java
@@ -9,20 +9,88 @@ import javafx.scene.media.Media
 import javafx.scene.media.MediaPlayer
 */
 
-public class recursiveWordSearch {
+import java.util.*;
+import java.io.File; 
+import java.io.IOException; 
+import java.util.Scanner; 
+  
+import javax.sound.sampled.AudioInputStream; 
+import javax.sound.sampled.AudioSystem; 
+import javax.sound.sampled.Clip; 
+import javax.sound.sampled.LineUnavailableException; 
+import javax.sound.sampled.UnsupportedAudioFileException; 
+
+public class recursiveGridSearch {
 	static boolean found = false;
 	
 	public static void main(String args[]) {
 		String[][] board = {{"B", "A", "D", "E", "L"}, 
 							{"Q", "D", "W", "P", "O"}, 
-							{"F", "F", "F", "N", "T"}, 
+							{"F", "I", "F", "N", "T"}, 
 							{"L", "O", "F", "A", "S"}, 
 							{"V", "H", "K", "S", "N"}};
 
-		String word = "VOA"; 
+		String word = "ADEL";
 		
 		System.out.println(checkAdjacent(board, word));
-				
+	}
+	
+	public static boolean validate(String word, int wordLen, String[] wordlist, ArrayList<String> wordsEntered, String[][] board) {
+		int min = 0;
+		int max = wordlist.length-1;
+		//int posistion  = checkDict(wordlist, word, min, max);
+		
+		if (checkDict(wordlist, word, min, max) > -1 && checkLength(word, wordLen) && checkAdjacent(board, word) && checkDuplicateWord(wordsEntered, word)) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public static int checkDict(String[] wordlist, String word, int min, int max) {
+		int middle = (max + min)/2;
+		
+		if (word.compareTo(wordlist[middle]) > 0) {
+			return checkDict(wordlist, word, middle+1, max);
+		} else if (word.compareTo(wordlist[middle]) < 0) {
+			return checkDict(wordlist, word, min, middle-1);
+		} else if (word.compareTo(wordlist[middle])==0) {
+			return middle;
+		}
+		
+		return -1;
+	}
+	
+	public static boolean checkLength(String word, int wordLen) {
+		if (word.length()>=wordLen) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public static boolean checkDuplicateWord(ArrayList<String> usedWords, String word) {
+		if (usedWords.contains(word)) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public static void startMusic(String filepath) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+		 
+        // create AudioInputStream object 
+		AudioSystem audioInputStream =  
+                new AudioSystem.getAudioInputStream(new File(filepath).getAbsoluteFile()); 
+          
+        // create clip reference 
+        Clip clip = AudioSystem.getClip(); 
+          
+        // open audioInputStream to the clip 
+        clip.open(audioInputStream); 
+          
+        clip.loop(Clip.LOOP_CONTINUOUSLY); 
+        clip.start();
 	}
 	
 	public static boolean checkAdjacent(String[][] board, String word) {
@@ -53,7 +121,7 @@ public class recursiveWordSearch {
 	
 	public static void gridSearch(String[][] board, int row, int col, int prevRow, int prevCol, String word, int index, int wordLen) {
 		int[] x = {-1, -1, -1, 0, 0, 1, 1, 1};
-		int[] y = {-1, 0, 1, -1, 1, -1, 0, 1}; 
+		int[] y = {-1, 0, 1, -1, 1, -1, 0, 1};
 		
 		if (index > wordLen || !board[row][col].equals(Character.toString(word.charAt(index)))) {
 			return;
